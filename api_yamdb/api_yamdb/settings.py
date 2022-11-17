9547101113eb1e0c46276e1087004a6e734ceaf3
rinko69 <rinko-69@yandex.ru>
2022-11-17 23:40:01 +0300

    Моя попытка номер пять...

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '51.250.31.16']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '51.250.31.16', '*']
 
 
 # Application definition
